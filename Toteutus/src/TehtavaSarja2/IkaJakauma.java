b68179c15b20d6d91a090b84e13165bae217d646
PatrickJaloScott <patrickjalo.scott@gmail.com>
2023-08-31 16:52:49 +0300

    Ikäjakauman kehitys
    Lisätty käyttäjä syöttö tilastomäärälle ja muokattu käyttämään sitä.
    Lisätty tuloksien virhelaskenta ja hienosäätö tulostukseen.

--- Toteutus/src/TehtavaSarja2/IkaJakauma.java
+++ Toteutus/src/TehtavaSarja2/IkaJakauma.java
@@ -1,17 +1,21 @@
 package TehtavaSarja2;
 
+import java.util.Scanner;
+
 public class IkaJakauma {
     // Taulukko arvot
     // satunnaisluvun arvovälit esitetty -min ja -max taulukoilla
     static int[] satunnaisValitMin = {0, 5, 20, 40, 70, 85, 95},
-        satunnaisValitMax = {4, 19, 39, 69, 84, 94, 99},
+            satunnaisValitMax = {4, 19, 39, 69, 84, 94, 99},
     // ikätaulukko
         iat = {19, 20, 21, 23, 25, 27, 30},
     // tuloksia varten käytettävä taulukko
         tulokset = new int[iat.length];
     public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        int opiskelijamaara = inputPrompt(lukija, "Montako ihmistä käytetään");
         // silmukoidaan arpoluvun generointi
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < opiskelijamaara; i++) {
             // luodaan arpoluku välistä [0, 100]
             int arpoluku = (int)(Math.random() * 100);
             // verrataan luotu arpoluku satunnaisväleihin,
@@ -26,18 +30,31 @@ public class IkaJakauma {
                 }
             }
         }
+        double kokonaisvirhe = 0;
         // tulostetaan tulokset
         for (int i = 0; i < tulokset.length; i++) {
-            // jaetaan tulos 10:llä
-            // ('tulos/1000' on desimaali nollan ja ykkösen välissä...
-            // joten 'tulos * 100 / 1000' voi kirjoittaa tulos/10)
-            double prosentti = tulokset[i]/10.0;
-            System.out.printf("Ikää %d tuli %d kertaa ja on %.1f prosenttia tuloksista\n", iat[i], tulokset[i], prosentti);
+            // jaetaan tulos prosentiksi
+            double prosentti = (double) (tulokset[i]*100) /opiskelijamaara,
+                    virhe = Math.abs((satunnaisValitMax[i]-satunnaisValitMin[i]+1)-prosentti);
+            kokonaisvirhe += virhe;
+            System.out.printf("Ikää %d tuli %d kertaa (%.1f%%)\tVirhe %.2f%%\n", iat[i], tulokset[i], prosentti, virhe);
         }
+        kokonaisvirhe /= tulokset.length;
+        System.out.printf("\nTuloksista tuli %.2f%% keskimääräinen kokonaisvirhe", kokonaisvirhe);
     }
 
     // apufunktio jolla verrataan luku min-max väliin
     static Boolean onValissa(int x, int min, int max) {
-        return (min < x && x < max);
+        return (min <= x && x <= max);
+    }
+
+    public static int inputPrompt(String message) {
+        System.out.print(message + ": ");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static int inputPrompt(Scanner s, String message) {
+        System.out.print(message + ": ");
+        return s.nextInt();
     }
 }
